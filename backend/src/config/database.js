0c021f13a4e5091e792198fdff426893e226395f
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2019-07-29 17:03:05 -0300

    Finaliza backend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect("mongodb://localhost/money")
+module.exports = mongoose.connect("mongodb://localhost/money", { useNewUrlParser: true })
 
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'"
 mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'"
